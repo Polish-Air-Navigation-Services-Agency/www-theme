4d1b3f5fae4f551b857b554f38d56cd06f44f2d1
Tomek <TK-54-4B>
2024-08-18 14:10:34 +0200

    flexible content setup

--- theme/flexible-content/sections/hero.php
+++ theme/flexible-content/sections/hero.php
@@ -9,17 +9,15 @@ if ($hero_background['url']) {
 $hero_text = $args['hero_text'];
 ?>
 
-<?php if (!(is_account_page() && !is_user_logged_in())) : ?>
-    <div class="relative w-full h-[300px] md:h-[600px] md:!h-[400px] flex flex-col items-center justify-center">
-        <?php if (isset($hero_bg_url)) : ?>
-            <img src="<?php echo $hero_bg_url ?>" alt="background_image">
+<div class="relative w-full h-[300px] md:h-[600px] md:!h-[400px] flex flex-col items-center justify-center">
+    <?php if (isset($hero_bg_url)) : ?>
+        <img src="<?php echo $hero_bg_url ?>" alt="background_image">
+    <?php endif; ?>
+
+    <div class="relative z-0 flex flex-col items-center justify-center container">
+        <?php if ($hero_text) : ?>
+            <h1 class="<?php if ($display_logo) : ?>text-3xl md:text-[46px]<?php else : ?>text-5xl md:text-[46px] max-w-[760px] [.woocommerce-cart_&]:max-w-[880px] md:leading-[55px]<?php endif; ?> text-bold text-white font-bold text-center"><?php echo $hero_text; ?></h1>
         <?php endif; ?>
-
-        <div class="relative z-0 flex flex-col items-center justify-center container">
-            <?php if ($hero_text) : ?>
-                <h1 class="<?php if ($display_logo) : ?>text-3xl md:text-[46px]<?php else : ?>text-5xl md:text-[46px] max-w-[760px] [.woocommerce-cart_&]:max-w-[880px] md:leading-[55px]<?php endif; ?> text-bold text-white font-bold text-center"><?php echo $hero_text; ?></h1>
-            <?php endif; ?>
-        </div>
-
     </div>
-<?php endif; ?>
+
+</div>
