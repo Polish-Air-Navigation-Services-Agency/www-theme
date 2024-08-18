341a42dd8f2998104805d2c2223e9bbc0ac6b99f
Tomek <TK-54-4B>
2024-08-18 14:17:05 +0200

    flexible content setup

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -1,18 +1,54 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 // Set the Preflight flag based on the build target.
 const includePreflight = 'editor' === process.env._TW_TARGET ? false : true;
 
 module.exports = {
+	experimental: {
+		optimizeUniversalDefaults: true,
+	},
 	presets: [
 		// Manage Tailwind Typography's configuration in a separate file.
 		require('./tailwind-typography.config.js'),
 	],
 	content: [
-		// Ensure changes to PHP files trigger a rebuild.
+		// Ensure changes to PHP files and `theme.json` trigger a rebuild.
 		'./theme/**/*.php',
 	],
 	theme: {
 		// Extend the default Tailwind theme.
-		extend: {},
+		container: {
+			center: true,
+			padding: {
+				DEFAULT: '1.25rem',
+				lg: '2rem',
+				xl: '4rem',
+				'2xl': '6rem',
+				'3xl': '10rem',
+			},
+		},
+		extend: {
+			fontFamily: {
+				sans: ['Raleway', ...defaultTheme.fontFamily.sans],
+			},
+			fontSize: {
+				xl: '1.375rem',
+				'5xl': ['2.625rem', '2.625rem'],
+			},
+			screens: {
+				lg: '1180px',
+				xl: '1380px',
+				'2xl': '1680px',
+				'3xl': '1920px',
+			},
+			backgroundSize: {
+				'size-200': '200% 200%',
+			},
+			backgroundPosition: {
+				'pos-0': '0% 0%',
+				'pos-100': '100% 100%',
+			},
+		},
 	},
 	corePlugins: {
 		// Disable Preflight base styles in builds targeting the editor.
--- theme/flexible-content/sections/hero.php
+++ theme/flexible-content/sections/hero.php
@@ -11,12 +11,12 @@ $hero_text = $args['hero_text'];
 
 <div class="relative w-full h-[300px] md:h-[600px] md:!h-[400px] flex flex-col items-center justify-center">
     <?php if (isset($hero_bg_url)) : ?>
-        <img src="<?php echo $hero_bg_url ?>" alt="background_image">
+        <img src="<?php echo $hero_bg_url ?>" alt="background_image" class="absolute inset-0 z-[-1] object-cover !h-full w-full">
     <?php endif; ?>
 
     <div class="relative z-0 flex flex-col items-center justify-center container">
         <?php if ($hero_text) : ?>
-            <h1 class="<?php if ($display_logo) : ?>text-3xl md:text-[46px]<?php else : ?>text-5xl md:text-[46px] max-w-[760px] [.woocommerce-cart_&]:max-w-[880px] md:leading-[55px]<?php endif; ?> text-bold text-white font-bold text-center"><?php echo $hero_text; ?></h1>
+            <h1 class="text-[40px] lg:text-[80px] text-bold text-white font-bold text-center"><?php echo $hero_text; ?></h1>
         <?php endif; ?>
     </div>
 
