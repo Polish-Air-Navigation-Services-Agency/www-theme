bc55b716eac26b486c402b6892d6b67c7177a62d
Tomek <TK-54-4B>
2024-08-18 15:44:45 +0200

    hero styles

--- theme/flexible-content/sections/hero.php
+++ theme/flexible-content/sections/hero.php
@@ -9,17 +9,15 @@ if ($hero_background['url']) {
 $hero_text = $args['hero_text'];
 ?>
 
-<div class="absolute top-0">
-    <div class="relative w-full h-[300px] md:h-[600px] md:!h-[700px] flex flex-col items-center justify-center">
-        <?php if (isset($hero_bg_url)) : ?>
-            <img src="<?php echo $hero_bg_url ?>" alt="background_image" class="absolute inset-0 z-[-1] object-cover !h-full w-full">
+<div class="relative translate-y-[-88px] w-full h-[300px] md:h-[600px] md:!h-[700px] flex flex-col items-center justify-center">
+    <?php if (isset($hero_bg_url)) : ?>
+        <img src="<?php echo $hero_bg_url ?>" alt="background_image" class="absolute inset-0 z-[-1] object-cover !h-full w-full">
+    <?php endif; ?>
+
+    <div class="relative z-0 flex flex-col items-center justify-center container">
+        <?php if ($hero_text) : ?>
+            <h1 class="text-[40px] lg:text-[80px] lg:leading-20 font-medium text-white"><?php echo $hero_text; ?></h1>
         <?php endif; ?>
-
-        <div class="relative z-0 flex flex-col items-center justify-center container">
-            <?php if ($hero_text) : ?>
-                <h1 class="text-[40px] lg:text-[80px] lg:leading-20 font-medium text-white"><?php echo $hero_text; ?></h1>
-            <?php endif; ?>
-        </div>
-
     </div>
+
 </div>
