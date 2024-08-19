672e2c7ecd378542365209d02950219d9f42b668
Tomek <TK-54-4B>
2024-08-19 19:56:17 +0200

    section counters styles

--- theme/flexible-content/sections/section-counters.php
+++ theme/flexible-content/sections/section-counters.php
@@ -6,7 +6,7 @@ $tiles_list = $args['tiles_list'];
 
 ?>
 
-<div class="relative w-full bg-primary py-[140px] pb-20 md:pt-11 mt-10 mb-20">
+<div class="relative w-full bg-primary py-[140px]">
     <div class="decoration absolute top-0 left-1/2 -translate-x-1/2">
         <svg xmlns="http://www.w3.org/2000/svg" width="150" height="75" viewBox="0 0 150 75" fill="none">
             <circle cx="75" r="75" fill="#60B8D1" />
@@ -23,14 +23,16 @@ $tiles_list = $args['tiles_list'];
                     <div class="relative flex flex-col items-center text-left">
                         <div class="mb-5 md:mb-7 self-start">
                             <?php if ($tile) : ?>
-                                <h3 class="mb-6 text-2xl lg:text-[60px] text-white font-medium">
-                                    <span id="target<?php echo $tileID ?>" data-counter-target<?php echo $tileID ?>="<?php echo $tile['counter_target']; ?>" data-counter-qty class="text-2xl lg:text-3xl xl:text-4xl font-bold"><?php echo $tile['counter_target']; ?></span>
+                                <h3 class="mb-6 text-[40px] lg:text-[60px] text-white font-medium">
+                                    <span id="target<?php echo $tileID ?>" data-counter-target<?php echo $tileID ?>="<?php echo $tile['counter_target']; ?>" data-counter-qty>
+                                        <?php echo $tile['counter_target']; ?>
+                                    </span>
                                     <span class="text-secondary text-[16px] font-semibold"><sup><?php echo $tile['counter_unit']; ?></sup></span>
                                 </h3>
                             <?php endif; ?>
                         </div>
                         <?php if ($tile['description']) : ?>
-                            <p class="text-[14px] leading-[21px]"><?php echo $tile['description']; ?></p>
+                            <p class="text-[14px] text-white leading-[21px]"><?php echo $tile['description']; ?></p>
                         <?php endif; ?>
                     </div>
                 <?php endforeach; ?>
