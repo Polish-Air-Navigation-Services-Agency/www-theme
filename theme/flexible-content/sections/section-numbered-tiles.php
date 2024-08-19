2b44a5a43dfb16cda15c0782d978c9e4788d2f82
Tomek <TK-54-4B>
2024-08-19 22:06:48 +0200

    section numbered tiles styles

--- theme/flexible-content/sections/section-numbered-tiles.php
+++ theme/flexible-content/sections/section-numbered-tiles.php
@@ -9,7 +9,7 @@ $tiles = $args['tiles'];
 ?>
 
 <section class="bg-[#F6F8F9]">
-    <div class="container mx-auto pt-10">
+    <div class="container mx-auto pt-10 pb-[100px]">
         <div class="title-wrapper w-full flex flex-col justify-center items-center">
             <?php if ($section_number) : ?>
                 <p class="w-fit mb-2 text-[16px] leading-[24px] text-secondary font-semibold before:content-[''] before:w-[61px] before:h-[2px] before:bg-secondary before:rounded-full before:absolute before:top-1/2 before:-translate-y-1/2 before:left-[-70px] after:content-[''] after:w-[61px] after:h-[2px] after:bg-secondary after:rounded-full after:absolute after:top-1/2 after:-translate-y-1/2 after:right-[-70px] relative"><?php echo $section_number; ?></p>
@@ -28,8 +28,8 @@ $tiles = $args['tiles'];
                     $tileID++;
                 ?>
                     <?php if ($tile) : ?>
-                        <div class="tile-wrapper w-[355px] mx-auto px-5 py-[30px] flex flex-col <?php if ($tileID == 1) echo 'bg-primary';
-                                                                                                else echo 'bg-white'; ?>">
+                        <div class="tile-wrapper w-full lg:w-[355px] mx-auto px-5 py-[30px] flex flex-col <?php if ($tileID == 1) echo 'bg-primary';
+                                                                                                            else echo 'bg-white'; ?>">
                             <?php if ($tile['number']) : ?>
                                 <p class="size-[92px] mb-[90px] self-end flex flex-col justify-center items-center text-[60px] text-primary leading-[72px] font-medium  <?php if ($tileID == 1) echo 'bg-secondary rounded-full';
                                                                                                                                                                         else echo 'text-primary'; ?>"><?php echo $tile['number']; ?></p>
