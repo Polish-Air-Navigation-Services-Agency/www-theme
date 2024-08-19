c3c288ff9e922b17f4cc73041396168dececd431
Tomek <TK-54-4B>
2024-08-19 16:36:59 +0200

    header styles

--- theme/template-parts/layout/header-content.php
+++ theme/template-parts/layout/header-content.php
@@ -24,7 +24,7 @@
 			</a>
 		</div>
 
-		<div id="site-navigation" class="absolute lg:static inset-x-0 top-full h-0 lg:h-auto overflow-auto lg:overflow-visible bg-white shadow-none transition-all duration-500">
+		<div id="site-navigation" class="bg-transparent text-white absolute lg:static inset-x-0 top-full h-0 lg:h-auto overflow-auto lg:overflow-visible bg-white shadow-none transition-all duration-500">
 			<nav id="site-navigation" class="container lg:!px-0 pb-5 lg:pb-0 lg:!max-w-full flex flex-col lg:flex-row lg:items-center gap-4 font-semibold" aria-label="<?php esc_attr_e('Main Navigation', '_pansa'); ?>">
 				<?php
 				wp_nav_menu(
@@ -36,6 +36,12 @@
 				);
 				?>
 			</nav><!-- #site-navigation -->
+			<a href="<?php echo esc_url(home_url('/kontakt')); ?>" class="bg-white rounded-full px-6 py-[11px] flex items-center justify-center gap-[10px] text-primary">
+				Skontaktuj sie
+				<svg xmlns="http://www.w3.org/2000/svg" width="34" height="16" viewBox="0 0 34 16" fill="none">
+					<path d="M26.4003 14.5996L33 7.99996M33 7.99996L26.4003 1.4003M33 7.99996L1.49994 8.00003" stroke="#0F304D" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+				</svg>
+			</a>
 		</div>
 	</div>
 
