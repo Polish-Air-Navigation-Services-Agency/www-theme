2f64373b68c8d7c7587e84e90bed3e91e322c4f5
Tomek <TK-54-4B>
2024-08-19 21:30:26 +0200

    section numbered tiles styles

--- theme/flexible-content/sections/section-numbered-tiles.php
+++ theme/flexible-content/sections/section-numbered-tiles.php
@@ -2,7 +2,6 @@
 
 /** Template to display 'Sekcja z numerowanymi kafelkami' - section_numbered_tiles */
 
-$cols_reverse = $args['cols_reverse'];
 $section_number = $args['section_number'];
 $title = $args['title'];
 $tiles = $args['tiles'];
@@ -12,26 +11,30 @@ $tiles = $args['tiles'];
 <div>
     <div class="container">
         <div class="counter mb-[100px] lg:mb-[190px] grid grid-cols-1 lg:grid-cols-3 gap-[30px]">
-            <?php foreach ($tiles as $tile) :
-                $tileID++;
+            <?php if ($tiles_list) :
+                $tileID = 0;
             ?>
-                <?php if ($tile) : ?>
-                    <div class="tile-wrapper w-[355px] px-5 py-[30px] bg-primary">
-                        <?php if ($tile['number']) : ?>
-                            <p class="mb-[90px] text-[60px] text-primary leading-[72px] rounder-full bg-secondary"><?php echo $tile['number']; ?></p>
-                        <?php endif; ?>
+                <?php foreach ($tiles as $tile) :
+                    $tileID++;
+                ?>
+                    <?php if ($tile) : ?>
+                        <div class="tile-wrapper w-[355px] px-5 py-[30px] bg-primary">
+                            <?php if ($tile['number']) : ?>
+                                <p class="mb-[90px] text-[60px] text-primary leading-[72px] rounder-full bg-secondary"><?php echo $tile['number']; ?></p>
+                            <?php endif; ?>
 
-                        <?php if ($tile['title']) : ?>
-                            <p class="mb-[9px] text-[30px] text-white leading-[36px]"><?php echo $tile['title']; ?></p>
-                        <?php endif; ?>
+                            <?php if ($tile['title']) : ?>
+                                <p class="mb-[9px] text-[30px] text-white leading-[36px]"><?php echo $tile['title']; ?></p>
+                            <?php endif; ?>
 
-                        <?php if ($tile['description']) : ?>
-                            <p class="text-[16px] text-white leading-[24px]"><?php echo $tile['description']; ?></p>
-                        <?php endif; ?>
-                    </div>
-                <?php endif; ?>
+                            <?php if ($tile['description']) : ?>
+                                <p class="text-[16px] text-white leading-[24px]"><?php echo $tile['description']; ?></p>
+                            <?php endif; ?>
+                        </div>
+                    <?php endif; ?>
 
-            <?php endforeach; ?>
+                <?php endforeach; ?>
+            <?php endif; ?>
         </div>
     </div>
 </div>
