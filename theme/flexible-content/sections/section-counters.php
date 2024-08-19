8643d32f5de70e824aa90981f3bde05922eb2a91
Tomek <TK-54-4B>
2024-08-19 20:06:27 +0200

    section counters styles

--- theme/flexible-content/sections/section-counters.php
+++ theme/flexible-content/sections/section-counters.php
@@ -27,7 +27,11 @@ $tiles_list = $args['tiles_list'];
                                     <span id="target<?php echo $tileID ?>" data-counter-target<?php echo $tileID ?>="<?php echo $tile['counter_target']; ?>" data-counter-qty>
                                         <?php echo $tile['counter_target']; ?>
                                     </span>
-                                    <span><?php echo $tile['counter_unit2']; ?></span>
+
+                                    <?php if ($tile['counter_unit2']) : ?>
+                                        <span><?php echo $tile['counter_unit2']; ?></span>
+                                    <?php endif; ?>
+
                                     <span class="text-secondary text-[16px] font-semibold absolute top-4"><?php echo $tile['counter_unit']; ?></span>
                                 </h3>
                             <?php endif; ?>
