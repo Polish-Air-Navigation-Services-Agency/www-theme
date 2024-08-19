7ce9ef32a7e0bca2a801f794e058713e1b3aceaf
Tomek <TK-54-4B>
2024-08-19 21:06:47 +0200

    section counters two cols styles

--- theme/flexible-content/sections/section-counters.php
+++ theme/flexible-content/sections/section-counters.php
@@ -27,7 +27,7 @@ if ($section_img['url']) {
         <?php if ($tiles_list) :
             $tileID = 0;
         ?>
-            <div class="counter mb-[100px] lg:mb-[140px] grid grid-cols-1 lg:grid-cols-4 gap-x-10 gap-y-14 lg:gap-y-10">
+            <div class="counter mb-[100px] lg:mb-[190px] grid grid-cols-1 lg:grid-cols-4 gap-x-10 gap-y-14 lg:gap-y-10">
                 <?php foreach ($tiles_list as $tile) :
                     $tileID++;
                 ?>
@@ -56,7 +56,13 @@ if ($section_img['url']) {
         <?php endif; ?>
         <!-- /counter end -->
 
-        <div class="two-col flex basis">
+        <div class="two-col flex basis relative">
+            <div class="decoration absolute top-0 left-1/2 -translate-x-1/2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="779" height="175" viewBox="0 0 779 175" fill="none">
+                    <path d="M0.895508 1.25024C48.5167 49.1714 209.119 108.915 292.86 86.4769C332.215 75.9315 355.67 56.6011 351.951 39.1184C348.188 21.4161 307.858 25.5623 291.385 35.8969C279.023 43.6515 288.48 82.2306 303.245 111.424C320.558 145.657 357.34 177.982 398.767 166.882C441.574 155.411 461.15 114.684 449.802 72.3318C441.005 39.5019 418.242 0.63599 397.479 6.19948C376.716 11.7627 373.346 54.1068 382.721 89.0967C404.877 171.782 506.862 191.156 625.875 159.395C626.04 159.351 710.5 141 778 25" stroke="#60B8D1" stroke-width="2" />
+                </svg>
+            </div>
+
             <div class="col1 basis-1/2 flex-col">
                 <?php if ($section_number) : ?>
                     <p class="w-fit mb-2 text-[16px] leading-[24px] text-secondary font-semibold after:content-[''] after:w-[61px] after:h-[2px] after:bg-secondary after:rounded-full after:absolute after:top-1/2 after:-translate-y-1/2 after:right-[-70px] relative"><?php echo $section_number; ?></p>
