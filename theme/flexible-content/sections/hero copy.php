328584036cddcb2aa2fe2264aa9028a4fd00807f
Tomek <TK-54-4B>
2024-08-19 18:33:54 +0200

    section about styles

---
+++ theme/flexible-content/sections/hero copy.php
@@ -0,0 +1,23 @@
+<?php
+
+/** Template to display 'Sekcja z treścią na dwie kolumny' - section_two_cols */
+
+$section_number = $args['section_number'];
+$title = $args['title'];
+$button = $args['button'];
+$description = $args['description'];
+
+?>
+
+<div>
+    <div class="container w-full flex">
+        <div class="col1 basis-1/2">
+            <?php echo $section_number; ?>
+            <?php echo $title; ?>
+            <?php echo $button; ?>
+        </div>
+        <div class="col2 basis-1/2">
+            <?php echo $description; ?>
+        </div>
+    </div>
+</div>
