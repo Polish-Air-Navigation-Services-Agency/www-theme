fd0c7c9177e8690154b8eb75ea66e105a413d70f
Tomek <TK-54-4B>
2024-08-19 16:38:18 +0200

    header styles

--- theme/template-parts/layout/header-content.php
+++ theme/template-parts/layout/header-content.php
@@ -35,13 +35,14 @@
 					)
 				);
 				?>
+				<a href="<?php echo esc_url(home_url('/kontakt')); ?>" class="bg-white rounded-full px-6 py-[11px] flex items-center justify-center gap-[10px] text-primary">
+					Skontaktuj sie
+					<svg xmlns="http://www.w3.org/2000/svg" width="34" height="16" viewBox="0 0 34 16" fill="none">
+						<path d="M26.4003 14.5996L33 7.99996M33 7.99996L26.4003 1.4003M33 7.99996L1.49994 8.00003" stroke="#0F304D" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+					</svg>
+				</a>
 			</nav><!-- #site-navigation -->
-			<a href="<?php echo esc_url(home_url('/kontakt')); ?>" class="bg-white rounded-full px-6 py-[11px] flex items-center justify-center gap-[10px] text-primary">
-				Skontaktuj sie
-				<svg xmlns="http://www.w3.org/2000/svg" width="34" height="16" viewBox="0 0 34 16" fill="none">
-					<path d="M26.4003 14.5996L33 7.99996M33 7.99996L26.4003 1.4003M33 7.99996L1.49994 8.00003" stroke="#0F304D" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-				</svg>
-			</a>
+
 		</div>
 	</div>
 
