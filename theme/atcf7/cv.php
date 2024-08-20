1b9ad0045923f138449eaaea9b71d8cf3e27ae36
Tomek <TK-54-4B>
2024-08-20 18:42:01 +0200

    contact form styles

--- theme/atcf7/cv.php
+++ theme/atcf7/cv.php
@@ -47,7 +47,7 @@ if (!defined('ABSPATH')) {
     </div>
   </div>
 
-  <div class="btn-submit group relative flex gap-[10px] text-center max-w-full w-fit mx-auto [&_.wpcf7-spinner]:absolute [&_.wpcf7-spinner]:right-[-7px] [&_.wpcf7-spinner]:top-4 [&_svg_path]:hover:fill-secondary [&_svg_circle]:hover:stroke-secondary">
+  <div class="btn-submit group relative flex gap-[10px] text-center max-w-full w-fit mx-auto [&_.wpcf7-spinner]:absolute [&_.wpcf7-spinner]:right-[-7px] [&_.wpcf7-spinner]:top-4 [&_svg_path]:hover:fill-secondary [&_svg_circle]:hover:stroke-secondary [&>input]:bg-primary hover:bg-white !rounded-full !px-6 !py-[11px] text-white hover:text-[#0F304D] transition ease duration-200">
     [submit "Wyślij"]
     <svg class="self-center" xmlns="http://www.w3.org/2000/svg" width="34" height="16" viewBox="0 0 34 16" fill="none">
       <path class="group-hover:stroke-white" d="M26.4003 14.5996L33 7.99996M33 7.99996L26.4003 1.4003M33 7.99996L1.49994 8.00003" stroke="#0F304D" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
