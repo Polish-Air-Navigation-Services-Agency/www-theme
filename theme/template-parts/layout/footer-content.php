a8d3a7186cfac3eceb9c2e7f96f55d5d776edb37
Tomek <TK-54-4B>
2024-08-20 21:35:53 +0200

    footer styles

--- theme/template-parts/layout/footer-content.php
+++ theme/template-parts/layout/footer-content.php
@@ -11,19 +11,36 @@
 ?>
 
 <footer id="colophon" class="py-10 flex items-center justify-center">
-	<p>footer</p>
-	<?php if (has_nav_menu('menu-2')) : ?>
-		<nav aria-label="<?php esc_attr_e('Footer Menu', '_pansa'); ?>">
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-2',
-					'menu_class'     => 'footer-menu',
-					'depth'          => 1,
-				)
-			);
-			?>
-		</nav>
-	<?php endif; ?>
+	<div>
+		<?php if (has_nav_menu('menu-2')) : ?>
+			<nav aria-label="<?php esc_attr_e('Footer Menu', '_pansa'); ?>">
+				<?php
+				wp_nav_menu(
+					array(
+						'theme_location' => 'menu-2',
+						'menu_class'     => 'footer-menu',
+						'depth'          => 1,
+					)
+				);
+				?>
+			</nav>
+		<?php endif; ?>
+	</div>
+
+	<div>
+		<?php if (has_nav_menu('menu-2')) : ?>
+			<nav aria-label="<?php esc_attr_e('Footer Menu', '_pansa'); ?>">
+				<?php
+				wp_nav_menu(
+					array(
+						'theme_location' => 'menu-3',
+						'menu_class'     => 'footer-menu',
+						'depth'          => 1,
+					)
+				);
+				?>
+			</nav>
+		<?php endif; ?>
+	</div>
 
 </footer><!-- #colophon -->
