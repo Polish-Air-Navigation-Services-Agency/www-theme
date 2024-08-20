183138b3683a06c824ddc9483827950f773acad6
Tomek <TK-54-4B>
2024-08-20 21:58:05 +0200

    footer styles

--- theme/template-parts/layout/footer-content.php
+++ theme/template-parts/layout/footer-content.php
@@ -15,8 +15,8 @@
 		<div class="footer-header mb-[60px] flex basis">
 			<h2 class="basis-1/2 text-[50px] lg:text-[60px] text-white leading-[64px] lg:leading-[72px] font-medium"><?php echo esc_html_e('Nie widzisz oferty pracy dla siebie?', '_pansa'); ?></h2>
 			<div class="basis-1/2">
-				<div class="w-full flex justify-end">
-					<button class="group self-end bg-white hover:bg-primary !rounded-full !px-6 !py-[11px] text-[#0F304D] font-medium hover:text-white transition ease duration-200">
+				<div class="w-full flex justify-end items-center">
+					<button class="group self-center bg-white hover:bg-primary !rounded-full !px-6 !py-[11px] text-[#0F304D] font-medium hover:text-white transition ease duration-200">
 						<a href="<?php echo esc_url(home_url('/kontakt')); ?>" class="flex items-center justify-center gap-[10px]">
 							<?php esc_html_e('Skontaktuj się z nami', '_pansa'); ?>
 							<svg class="self-center" xmlns="http://www.w3.org/2000/svg" width="34" height="16" viewBox="0 0 34 16" fill="none">
