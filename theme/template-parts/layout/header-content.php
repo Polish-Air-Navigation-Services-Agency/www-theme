60692d8311e07b65ee22f4c5634c59f94a0b4ca3
Tomek <TK-54-4B>
2024-08-20 18:20:57 +0200

    btn hover added

--- theme/template-parts/layout/header-content.php
+++ theme/template-parts/layout/header-content.php
@@ -35,7 +35,7 @@
 					)
 				);
 				?>
-				<button class="bg-white !rounded-full !px-6 !py-[11px] text-[#0F304D]">
+				<button class="bg-white hover:bg-primary !rounded-full !px-6 !py-[11px] text-[#0F304D] hover:text-white transition ease duration-200">
 					<a href="<?php echo esc_url(home_url('/kontakt')); ?>" class="flex items-center justify-center gap-[10px]">
 						<?php esc_html_e('Skontaktuj się', '_pansa'); ?>
 						<svg class="self-center" xmlns="http://www.w3.org/2000/svg" width="34" height="16" viewBox="0 0 34 16" fill="none">
