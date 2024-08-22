13aae3288a648fb8a641127b16ce1b4824331932
Tomek <TK-54-4B>
2024-08-22 16:47:34 +0200

    hero, added btn hover

--- theme/flexible-content/sections/hero.php
+++ theme/flexible-content/sections/hero.php
@@ -39,11 +39,11 @@ if ($hero_background['url']) {
             <?php endif; ?>
         </div>
 
-        <button class="bg-white w-fit !rounded-full !px-6 !py-[11px] text-[#0F304D] font-medium">
+        <button class="group bg-white w-fit !rounded-full !px-6 !py-[11px] text-[#0F304D] font-medium hover:bg-primary hover:text-white">
             <a href="<?php echo esc_url(home_url('/kogo-szukamy')); ?>" class="flex items-center justify-center gap-[10px]">
                 <?php esc_html_e('Sprawdź kogo szukamy', '_pansa'); ?>
                 <svg class="self-center" xmlns="http://www.w3.org/2000/svg" width="34" height="16" viewBox="0 0 34 16" fill="none">
-                    <path d="M26.4003 14.5996L33 7.99996M33 7.99996L26.4003 1.4003M33 7.99996L1.49994 8.00003" stroke="#0F304D" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    <path class="group-hover:stroke-white" d="M26.4003 14.5996L33 7.99996M33 7.99996L26.4003 1.4003M33 7.99996L1.49994 8.00003" stroke="#0F304D" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                 </svg>
             </a>
         </button>
