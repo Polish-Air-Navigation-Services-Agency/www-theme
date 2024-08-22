3d45c6bb59926cf4a8de6562c3bcb316fa586d3b
Tomek <TK-54-4B>
2024-08-22 18:23:20 +0200

    mobile menu styles

--- theme/template-parts/layout/header-content.php
+++ theme/template-parts/layout/header-content.php
@@ -32,6 +32,13 @@
 			</svg>
 		</button>
 
+		<svg class="ham hamRotate ham8" viewBox="0 0 100 100" width="80" onclick="this.classList.toggle('active')">
+			<path class="line top" d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20" />
+			<path class="line middle" d="m 30,50 h 40" />
+			<path class="line bottom" d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20" />
+		</svg>
+
+
 		<div id="site-navigation" class="w-full bg-[#055bc0] lg:bg-transparent text-white absolute lg:static inset-x-0 top-full h-0 lg:h-auto overflow-auto lg:overflow-visible shadow-none transition-all duration-500" data-js="nav-container">
 			<nav class="container w-full pt-5 lg:pt-0 lg:!px-0 pb-5 lg:pb-0 lg:!max-w-full flex flex-col lg:flex-row lg:items-center lg:justify-between gap-4 font-medium" aria-label="<?php esc_attr_e('Main Navigation', '_pansa'); ?>">
 				<?php
