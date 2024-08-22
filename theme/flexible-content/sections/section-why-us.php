707051df701283e99706f79071d37cb912f32d2e
Tomek <TK-54-4B>
2024-08-22 20:26:22 +0200

    why us video styles

--- theme/flexible-content/sections/section-why-us.php
+++ theme/flexible-content/sections/section-why-us.php
@@ -35,13 +35,10 @@ if ($video_thumbnail['url']) {
         <div class="video-wrapper lg:w-full lg:h-[630x] [&>iframe]:w-full [&>iframe]:lg:h-[630px] video-player-wrapper relative after:z-[1] after:content-[''] after:absolute after:top-0 after:left-0 after:w-full after:transition after:duration-[300ms] after:ease-out after:bg-center after:bg-cover after:bg-no-repeat after:h-[630px]" data-videoWrapper>
             <?php echo $video; ?>
             <div class="wp-block-group btn-more js-videoBtn group desktop:w-[175px] w-[140px] desktop:h-[175px] h-[140px] mx-auto text-center rounded-full z-[2] absolute top-[60%] desktop:top-[60%] left-[50%] translate-x-[-50%] translate-y-[-80%] transition ease-out duration-300 flex flex-col justify-center items-center gap-y-0 cursor-pointer overflow-hidden">
-                <div class="wp-block-group yellow-bg w-full h-full rounded-full bg-primaryYellow scale-100 group-hover:scale-[85%] absolute transition ease-in-out duration-[700ms] z-0">
-                    <p class="hidden after:z-[-1]">yellow bg</p>
-                </div>
-                <div class="wp-block-group white-bg w-full h-full rounded-full bg-white scale-0 group-hover:scale-[85%] absolute transition ease-in-out duration-[500ms] z-[1]">
-                    <p class="hidden">white bg</p>
-                </div>
-                <p class="z-[2] text-white text-[35px] group-hover:text-black transition ease-in-out duration-[550ms]"><span class="font-bold block">PLAY</span><span class="font-bold block text-[16px]">VIDEO</span></p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100" fill="none">
+                    <rect width="100" height="100" rx="50" fill="white" fill-opacity="0.4" />
+                    <path d="M70 50L40 67.3205L40 32.6795L70 50Z" fill="white" />
+                </svg>
             </div>
         </div>
         <div class="desc-wrapper lg:w-[457px] text-white bg-primary pt-[70px] pb-10 px-5 lg:px-10 lg:absolute bottom-[-80px] right-0 z-[3]">
