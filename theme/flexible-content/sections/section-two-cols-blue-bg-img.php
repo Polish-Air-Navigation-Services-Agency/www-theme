60c0c1b9ddfa76d8531a5260d58390eaab05350e
Tomek <TK-54-4B>
2024-09-16 20:56:27 +0200

    section two cols blue bg fixing

--- theme/flexible-content/sections/section-two-cols-blue-bg-img.php
+++ theme/flexible-content/sections/section-two-cols-blue-bg-img.php
@@ -28,13 +28,13 @@ if ($section_img['url']) {
 
     <!-- banner img -->
     <?php if (isset($banner_img)) : ?>
-        <img src="<?php echo $banner_img_url; ?>" alt="background image" class="absolute inset-0 z-[-1] object-cover !h-full w-full">
+        <img src="<?php echo $banner_img_url; ?>" alt="background image" class="">
 
     <?php endif; ?>
     <!-- /banner img end -->
 
     <div class="pt-12 relative z-0 container">
-        <div class="two-col flex flex-col lg:flex-row basis relative">
+        <div class="two-col flex flex-col-reverse lg:flex-row basis relative">
             <div class="decoration hidden lg:block absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[-1]">
                 <svg xmlns="http://www.w3.org/2000/svg" width="779" height="175" viewBox="0 0 779 175" fill="none">
                     <path d="M0.895508 1.25024C48.5167 49.1714 209.119 108.915 292.86 86.4769C332.215 75.9315 355.67 56.6011 351.951 39.1184C348.188 21.4161 307.858 25.5623 291.385 35.8969C279.023 43.6515 288.48 82.2306 303.245 111.424C320.558 145.657 357.34 177.982 398.767 166.882C441.574 155.411 461.15 114.684 449.802 72.3318C441.005 39.5019 418.242 0.63599 397.479 6.19948C376.716 11.7627 373.346 54.1068 382.721 89.0967C404.877 171.782 506.862 191.156 625.875 159.395C626.04 159.351 710.5 141 778 25" stroke="#60B8D1" stroke-width="2" />
@@ -51,10 +51,6 @@ if ($section_img['url']) {
                 <?php endif; ?>
             </div>
 
-            <?php if (isset($section_img)) : ?>
-                <img class="lg:absolute mb-5 lg:mb-0" src="<?php echo $section_img_url ?>" alt="section_image" class="test">
-            <?php endif; ?>
-
             <div class="col2 basis-1/2">
                 <?php if ($description) : ?>
                     <div class="max-w-fill lg:max-w-[414px] mb-4 text-[20px] text-white leading-[30px]"><?php echo $description; ?></div>
@@ -65,5 +61,9 @@ if ($section_img['url']) {
                 <?php endif; ?>
             </div>
         </div>
+
+        <?php if (isset($section_img)) : ?>
+            <img class="" src="<?php echo $section_img_url ?>" alt="section_image" class="test">
+        <?php endif; ?>
     </div>
 </section>
