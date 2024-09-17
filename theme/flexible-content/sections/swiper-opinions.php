d86801b40576c0f0fe9b62d9ed25436a57b1a8ee
Tomek <TK-54-4B>
2024-09-17 21:33:48 +0200

    swiper opinions styles

--- theme/flexible-content/sections/swiper-opinions.php
+++ theme/flexible-content/sections/swiper-opinions.php
@@ -17,7 +17,7 @@ $opinions_list = $args['opinions_list'];
                 <div class="swiper-wrapper z-[0]">
                     <?php foreach ($opinions_list as $opinion) : ?>
                         <div class="swiper-slide">
-                            <div class="slide flex flex-row basis gap-6">
+                            <div class="slide flex lg:flex-row basis gap-6">
                                 <div class="left-col basis-1/2 bg-[#D4DEE4]">
                                     <img class="m-auto" src="<?php echo $opinion['image'] ?>" alt="author-image">
                                 </div>
@@ -35,13 +35,13 @@ $opinions_list = $args['opinions_list'];
                 </div>
 
             </div>
-            <div class="swiper-button-prev after:!content-[''] !left-[70px] top-1/2">
+            <div class="swiper-button-prev after:!content-[''] !left-[20px] lg:!left-[70px] top-1/2">
                 <svg class="rotate-180 scale-[2.7]" xmlns="http://www.w3.org/2000/svg" width="72" height="72" viewBox="0 0 72 72" fill="none">
                     <circle cx="36" cy="36" r="36" transform="matrix(-1.19249e-08 -1 -1 1.19249e-08 72 72)" fill="#60B8D1" />
                     <path d="M36.5337 44.5971L45.3332 35.7976M45.3332 35.7976L36.5337 26.998M45.3332 35.7976L23.867 35.7975" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                 </svg>
             </div>
-            <div class="swiper-button-next after:!content-[''] !right-[70px] top-1/2">
+            <div class="swiper-button-next after:!content-[''] !right-[20px] lg:!right-[70px] top-1/2">
                 <svg class="scale-[2.7]" xmlns="http://www.w3.org/2000/svg" width="72" height="72" viewBox="0 0 72 72" fill="none">
                     <circle cx="36" cy="36" r="36" transform="matrix(-1.19249e-08 -1 -1 1.19249e-08 72 72)" fill="#60B8D1" />
                     <path d="M36.5337 44.5971L45.3332 35.7976M45.3332 35.7976L36.5337 26.998M45.3332 35.7976L23.867 35.7975" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
