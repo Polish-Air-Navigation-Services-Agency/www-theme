2a19564645a1466430214d720ff3211cf92323e7
Tomek <TK-54-4B>
2024-09-17 21:14:36 +0200

    swiper opinions styles

--- theme/flexible-content/sections/swiper-opinions.php
+++ theme/flexible-content/sections/swiper-opinions.php
@@ -6,7 +6,7 @@ $opinions_list = $args['opinions_list'];
 
 ?>
 
-<section class="relative pb-[100px] overflow-hidden">
+<section class="pb-[100px] overflow-hidden">
     <div class="container mx-auto relative">
         <div class="swiper !max-w-[768px]" data-js="swiper-tiles-wide">
             <div class="swiper-wrapper ">
@@ -17,7 +17,7 @@ $opinions_list = $args['opinions_list'];
                                 <img class="m-auto" src="<?php echo $opinion['image'] ?>" alt="author-image">
                             </div>
 
-                            <div class="right-col basis-1/2 flex justify-center">
+                            <div class="right-col basis-1/2 flex flex-col justify-center">
                                 <p class="mb-[6px] text-[#0E181E] font-medium text-[30px]"><?php echo $opinion['author'] ?></p>
 
                                 <p class="mb-10 text-secondary text-[18px] font-medium"><?php echo $opinion['position'] ?></p>
@@ -30,7 +30,7 @@ $opinions_list = $args['opinions_list'];
             </div>
 
         </div>
-        <div class="swiper-button-prev after:!content-['']">
+        <div class="swiper-button-prev after:!content-[''] left-0 top-1/2">
             <svg class="rotate-180 scale-[2.7]" xmlns="http://www.w3.org/2000/svg" width="72" height="72" viewBox="0 0 72 72" fill="none">
                 <circle cx="36" cy="36" r="36" transform="matrix(-1.19249e-08 -1 -1 1.19249e-08 72 72)" fill="#60B8D1" />
                 <path d="M36.5337 44.5971L45.3332 35.7976M45.3332 35.7976L36.5337 26.998M45.3332 35.7976L23.867 35.7975" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
