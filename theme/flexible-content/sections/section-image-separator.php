f2f46a66a1cb5e77f58f92e256d8d0ba6e39c344
Tomek <TK-54-4B>
2024-09-17 19:30:54 +0200

    added new section image separator

--- theme/flexible-content/sections/section-image-separator.php
+++ theme/flexible-content/sections/section-image-separator.php
@@ -8,9 +8,11 @@ $image = $args['image'];
 
 <section class="relative pb-[100px] overflow-hidden">
     <div class="w-full relative">
-        <svg class="absolute bottom-0 left-1/2 -translate-x-1/2" xmlns="http://www.w3.org/2000/svg" width="150" height="75" viewBox="0 0 150 75" fill="none">
+        <svg class="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-full" xmlns="http://www.w3.org/2000/svg" width="150" height="75" viewBox="0 0 150 75" fill="none">
             <circle cx="75" r="75" fill="#60B8D1" />
         </svg>
-        <img src="<?php echo $image; ?>" alt="image">
+        <?php if ($image) : ?>
+            <img src="<?php echo $image; ?>" alt="image">
+        <?php endif; ?>
     </div>
 </section>
