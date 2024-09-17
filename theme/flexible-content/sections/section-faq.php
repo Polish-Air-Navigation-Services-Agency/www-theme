29ce51d14f6593d4abc12b5cf04e920dcfea16ab
Tomek <TK-54-4B>
2024-09-17 22:38:26 +0200

    faq section styles

--- theme/flexible-content/sections/section-faq.php
+++ theme/flexible-content/sections/section-faq.php
@@ -13,6 +13,9 @@ $questions_list = $args['questions_list'];
         <div class="faq-wrapper">
             <?php $question_qty = 1; ?>
             <?php foreach ($questions_list as $question) :
+                if ($question_qty < 10) :
+                    $question_qty = '0' . $question_qty;
+                endif;
             ?>
                 <div class="question-wrapper flex gap-[44px] border-b-[1px] border-[#BEBFBF]">
                     <p class="text-secondary text-[30px] font-medium"><?php echo $question_qty . '/'; ?></p>
