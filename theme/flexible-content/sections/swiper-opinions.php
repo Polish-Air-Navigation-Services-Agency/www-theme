44a8ed59a6899fc0a2295d5fbaf5e2c1b4d17f2e
Tomek <TK-54-4B>
2024-09-17 21:08:05 +0200

    swiper opinions styles

--- theme/flexible-content/sections/swiper-opinions.php
+++ theme/flexible-content/sections/swiper-opinions.php
@@ -28,13 +28,13 @@ $opinions_list = $args['opinions_list'];
                     </div>
                 <?php endforeach; ?>
             </div>
-            <div class="swiper-button-prev after:content-[]">
+            <div class="swiper-button-prev after:!content-['']">
                 <svg class="rotate-180" xmlns="http://www.w3.org/2000/svg" width="72" height="72" viewBox="0 0 72 72" fill="none">
                     <circle cx="36" cy="36" r="36" transform="matrix(-1.19249e-08 -1 -1 1.19249e-08 72 72)" fill="#60B8D1" />
                     <path d="M36.5337 44.5971L45.3332 35.7976M45.3332 35.7976L36.5337 26.998M45.3332 35.7976L23.867 35.7975" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                 </svg>
             </div>
-            <div class="swiper-button-next after:content-[]">
+            <div class="swiper-button-next after:!content-[]">
                 <svg xmlns="http://www.w3.org/2000/svg" width="72" height="72" viewBox="0 0 72 72" fill="none">
                     <circle cx="36" cy="36" r="36" transform="matrix(-1.19249e-08 -1 -1 1.19249e-08 72 72)" fill="#60B8D1" />
                     <path d="M36.5337 44.5971L45.3332 35.7976M45.3332 35.7976L36.5337 26.998M45.3332 35.7976L23.867 35.7975" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
