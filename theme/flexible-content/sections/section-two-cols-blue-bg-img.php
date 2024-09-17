062aa53eaf292c2ad3b99c0a2581e7886567e452
Tomek <TK-54-4B>
2024-09-17 17:10:24 +0200

    section two cols blue bg video

--- theme/flexible-content/sections/section-two-cols-blue-bg-img.php
+++ theme/flexible-content/sections/section-two-cols-blue-bg-img.php
@@ -31,7 +31,7 @@ if ($section_img['url']) {
         <svg class="decoration hidden lg:block absolute bottom-0 right-[80px]" xmlns="http://www.w3.org/2000/svg" width="609" height="107" viewBox="0 0 609 107" fill="none">
             <circle cx="304.5" cy="304.5" r="304.5" fill="#60B8D1" />
         </svg>
-        <div class="pt-12 relative z-0 container z-1">
+        <div class="pt-12 relative container z-[1]">
             <div class="two-col flex flex-col lg:flex-row-reverse gap-0 lg:gap-[117px] basis relative">
                 <div class="col1 basis-1/2 flex-col">
                     <?php if ($section_number) : ?>
