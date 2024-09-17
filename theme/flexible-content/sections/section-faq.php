1be5591cd11e12e25d9bb0d0712b996da028c037
Tomek <TK-54-4B>
2024-09-17 22:23:20 +0200

    added faq section

---
+++ theme/flexible-content/sections/section-faq.php
@@ -0,0 +1,26 @@
+<?php
+
+/** Template to display 'Sekcja z pytaniami' - section_faq */
+
+$section_id = $args['section_id'];
+$section_number = $args['section_number'];
+$questions_list = $args['questions_list'];
+
+?>
+
+<section id="<?php if ($section_id) : echo $section_id;
+                endif; ?>" class="relative pt-[50px] pb-[45px] overflow-hidden">
+
+    <div class="container w-full flex flex-col items-center justify-center">
+        <?php foreach ($questions_list as $question) :
+            $question_qty = 1
+        ?>
+            <?php echo $question_qty; ?>
+            <?php echo $question['question']; ?>
+            <?php echo $question['answer']; ?>
+        <?php
+            $question_qty++;
+        endforeach;
+        ?>
+    </div>
+</section>
