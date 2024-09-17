0197c1bad250bc1579e3b660cdc60e98148d9bf3
Tomek <TK-54-4B>
2024-09-17 21:29:25 +0200

    swiper opinions styles

--- theme/flexible-content/sections/swiper-opinions.php
+++ theme/flexible-content/sections/swiper-opinions.php
@@ -9,7 +9,7 @@ $opinions_list = $args['opinions_list'];
 <section class="pb-[100px] overflow-hidden">
     <div class="container mx-auto">
         <div class="wrapper relative">
-            <svg class="decoration absolute bottom-0 left-0 z-[-1]" xmlns="http://www.w3.org/2000/svg" width="207" height="218" viewBox="0 0 207 218" fill="none">
+            <svg class="decoration absolute bottom-[-70px] left-[150px] z-[-1]" xmlns="http://www.w3.org/2000/svg" width="207" height="218" viewBox="0 0 207 218" fill="none">
                 <path d="M23.1519 0.999858C61.7343 11.338 76.385 54.402 74.441 89.9499C72.7836 120.265 62.4816 155.653 48.9037 156.136C30.8105 156.782 -3.65451 141.367 1.53699 125.397C6.66292 109.624 34.5708 104.997 71.2242 114.818C114.933 126.53 166.122 170.199 205.52 217" stroke="#60B8D1" stroke-width="2" />
             </svg>
 
@@ -35,13 +35,13 @@ $opinions_list = $args['opinions_list'];
                 </div>
 
             </div>
-            <div class="swiper-button-prev after:!content-[''] left-0 top-1/2">
+            <div class="swiper-button-prev after:!content-[''] !left-[70px] top-1/2">
                 <svg class="rotate-180 scale-[2.7]" xmlns="http://www.w3.org/2000/svg" width="72" height="72" viewBox="0 0 72 72" fill="none">
                     <circle cx="36" cy="36" r="36" transform="matrix(-1.19249e-08 -1 -1 1.19249e-08 72 72)" fill="#60B8D1" />
                     <path d="M36.5337 44.5971L45.3332 35.7976M45.3332 35.7976L36.5337 26.998M45.3332 35.7976L23.867 35.7975" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                 </svg>
             </div>
-            <div class="swiper-button-next after:!content-['']">
+            <div class="swiper-button-next after:!content-[''] !right-[70px] top-1/2">
                 <svg class="scale-[2.7]" xmlns="http://www.w3.org/2000/svg" width="72" height="72" viewBox="0 0 72 72" fill="none">
                     <circle cx="36" cy="36" r="36" transform="matrix(-1.19249e-08 -1 -1 1.19249e-08 72 72)" fill="#60B8D1" />
                     <path d="M36.5337 44.5971L45.3332 35.7976M45.3332 35.7976L36.5337 26.998M45.3332 35.7976L23.867 35.7975" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
