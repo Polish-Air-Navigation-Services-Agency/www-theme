6086a35a848f9d3282ba6e7fff15ceab9237bf7d
Tomek <TK-54-4B>
2024-09-18 18:20:45 +0200

    reorganization blue bg sections

--- theme/flexible-content/sections/section-two-cols-image.php
+++ theme/flexible-content/sections/section-two-cols-image.php
@@ -12,11 +12,8 @@ $image = $args['image'];
 
 <section class="bg-primary">
     <div class="container mx-auto relativ mb-20 pt-[180px]">
-        <svg class="decoration hidden lg:block absolute bottom-0 right-[80px] z-[0]" xmlns="http://www.w3.org/2000/svg" width="609" height="107" viewBox="0 0 609 107" fill="none">
-            <circle cx="304.5" cy="304.5" r="304.5" fill="#60B8D1" />
-        </svg>
         <div class="two-col flex flex-col lg:flex-row basis relative">
-            <div class="decoration hidden lg:block absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[-1]">
+            <div class="decoration hidden lg:block absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[0]">
                 <svg xmlns="http://www.w3.org/2000/svg" width="779" height="175" viewBox="0 0 779 175" fill="none">
                     <path d="M0.895508 1.25024C48.5167 49.1714 209.119 108.915 292.86 86.4769C332.215 75.9315 355.67 56.6011 351.951 39.1184C348.188 21.4161 307.858 25.5623 291.385 35.8969C279.023 43.6515 288.48 82.2306 303.245 111.424C320.558 145.657 357.34 177.982 398.767 166.882C441.574 155.411 461.15 114.684 449.802 72.3318C441.005 39.5019 418.242 0.63599 397.479 6.19948C376.716 11.7627 373.346 54.1068 382.721 89.0967C404.877 171.782 506.862 191.156 625.875 159.395C626.04 159.351 710.5 141 778 25" stroke="#60B8D1" stroke-width="2" />
                 </svg>
