7baf9d48cce25669863bc529f7c4c20c95a3fc9e
Kamil <k.medrala99@gmail.com>
2024-09-18 15:21:43 +0200

    [PAZP-12] max-w-[500px] from 2-col section header set to 600

--- theme/flexible-content/sections/section-two-cols.php
+++ theme/flexible-content/sections/section-two-cols.php
@@ -34,7 +34,7 @@ $bgGray = $args['bgGray'];
             <?php endif; ?>
 
             <?php if ($title) : ?>
-                <h2 class="text-[42px] lg:text-[60px] text-primary leading-[50px] lg:leading-[72px] font-medium"><?php echo $title; ?></h2>
+                <h2 class="max-w-[600px] text-[42px] lg:text-[60px] text-primary leading-[50px] lg:leading-[72px] font-medium"><?php echo $title; ?></h2>
             <?php endif; ?>
 
             <?php if ($button) : ?>
