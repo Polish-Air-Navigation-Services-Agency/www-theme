eaf7821aa50a89dfac16e5611c47e8835e04fab5
Kamil <k.medrala99@gmail.com>
2024-09-19 15:52:32 +0200

    [PAZP-4] section 2 cols-image cleanup, rwd, decor fixes

--- theme/flexible-content/sections/section-two-cols-image.php
+++ theme/flexible-content/sections/section-two-cols-image.php
@@ -12,52 +12,46 @@ $numbers_row = $args['numbers_row'];
 
 ?>
 
-<section class="<?php echo ($white_bg) ? 'bg-white' : 'bg-primary'; ?> mb-20 pt-[180px] lg:grid grid-cols-2">
+<section class="<?php echo ($white_bg) ? 'bg-white' : 'bg-primary'; ?> lg:mb-20 pt-20 lg:pt-[180px] lg:grid grid-cols-2">
     <div class="col-span-full row-start-1">
         <div class="container mx-auto relative">
-            <div class="two-col flex flex-col lg:flex-row basis relative">
-                <div class="decoration hidden lg:block absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[0]">
-                    <svg width="320" height="269" viewBox="0 0 320 269" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M1.00488 267.164C61.6364 251.018 184.279 149.215 205.177 71.2241C214.998 34.5707 210.371 6.66282 194.598 1.53689C178.628 -3.65462 163.213 30.8104 163.859 48.9036C164.343 62.4815 199.73 72.7835 230.045 74.4409C265.593 76.3849 308.657 61.7342 318.995 23.1517" stroke="#60B8D1" stroke-width="2" />
-                    </svg>
-                </div>
+            <svg class="hidden lg:block absolute -top-24 left-[calc(50%_-_100px)]" width="320" height="269" viewBox="0 0 320 269" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M1.00488 267.164C61.6364 251.018 184.279 149.215 205.177 71.2241C214.998 34.5707 210.371 6.66282 194.598 1.53689C178.628 -3.65462 163.213 30.8104 163.859 48.9036C164.343 62.4815 199.73 72.7835 230.045 74.4409C265.593 76.3849 308.657 61.7342 318.995 23.1517" stroke="#60B8D1" stroke-width="2" />
+            </svg>
 
-                <div class="basis-1/2 flex-col">
-                    <?php if ($section_number) : ?>
-                        <p class="w-fit mb-2 text-[16px] leading-[24px] text-secondary font-semibold after:content-[''] after:w-[61px] after:h-[2px] after:bg-secondary after:rounded-full after:absolute after:top-1/2 after:-translate-y-1/2 after:right-[-70px] relative"><?php echo $section_number; ?></p>
-                    <?php endif; ?>
+            <div class="w-full lg:w-1/2">
+                <?php if ($section_number) : ?>
+                    <p class="w-fit mb-2 text-[16px] leading-[24px] text-secondary font-semibold after:content-[''] after:w-[61px] after:h-[2px] after:bg-secondary after:rounded-full after:absolute after:top-1/2 after:-translate-y-1/2 after:right-[-70px] relative"><?php echo $section_number; ?></p>
+                <?php endif; ?>
 
-                    <?php if ($title) : ?>
-                        <h2 class="mb-[50px] text-[50px] lg:text-[60px] <?php echo ($white_bg) ? 'text-primary' : 'text-white'; ?> leading-[64px] lg:leading-[72px] font-medium"><?php echo $title; ?></h2>
-                    <?php endif; ?>
+                <?php if ($title) : ?>
+                    <h2 class="mb-[50px] text-[50px] lg:text-[60px] <?php echo ($white_bg) ? 'text-primary' : 'text-white'; ?> leading-[64px] lg:leading-[72px] font-medium"><?php echo $title; ?></h2>
+                <?php endif; ?>
 
-                    <?php if ($description) : ?>
-                        <div class="max-w-fill <?php echo ($white_bg) ? 'lg:max-w-[540px]' : 'lg:max-w-[414px]'; ?> mb-4 text-[20px] <?php echo ($white_bg) ? 'text-textGray' : 'text-white'; ?> leading-[30px]"><?php echo $description; ?></div>
-                    <?php endif; ?>
+                <?php if ($description) : ?>
+                    <div class="max-w-fill <?php echo ($white_bg) ? 'lg:max-w-[540px]' : 'lg:max-w-[414px]'; ?> mb-4 text-[20px] <?php echo ($white_bg) ? 'text-textGray' : 'text-white'; ?> leading-[30px]"><?php echo $description; ?></div>
+                <?php endif; ?>
 
-                    <?php if ($description2) : ?>
-                        <div class="max-w-fill <?php echo ($white_bg) ? 'lg:max-w-[540px]' : 'lg:max-w-[414px]'; ?> mb-10 lg:mb-[150px] text-[16px] <?php echo ($white_bg) ? 'text-textGray' : 'text-white'; ?> leading-[24px]"><?php echo $description2; ?></div>
-                    <?php endif; ?>
+                <?php if ($description2) : ?>
+                    <div class="max-w-fill <?php echo ($white_bg) ? 'lg:max-w-[540px]' : 'lg:max-w-[414px]'; ?> mb-10 lg:mb-[150px] text-[16px] <?php echo ($white_bg) ? 'text-textGray' : 'text-white'; ?> leading-[24px]"><?php echo $description2; ?></div>
+                <?php endif; ?>
 
-                    <?php if ($numbers_row) : ?>
-                        <div class="pt-10 border-t-[1px] border-[#D4DEE4] flex gap-[104px]">
-                            <div class="tile1 text-primary">
-                                <p class="font-semibold">Clients satisfaction</p>
-                                <p class="relative text-[60px] font-medium">100<span class="text-secondary text-[36px] font-semibold absolute top-[6px]"><sup>%</sup></span></p>
-                            </div>
+                <?php if ($numbers_row) : ?>
+                    <div class="pt-10 border-t-[1px] border-[#D4DEE4] flex gap-[104px]">
+                        <div class="tile1 text-primary">
+                            <p class="font-semibold">Clients satisfaction</p>
+                            <p class="relative text-[60px] font-medium">100<span class="text-secondary text-[36px] font-semibold absolute top-[6px]"><sup>%</sup></span></p>
                         </div>
-                    <?php endif; ?>
-
-
-                </div>
-
+                    </div>
+                <?php endif; ?>
             </div>
+
         </div>
     </div>
 
     <?php if (isset($image)) : ?>
         <div class="col-start-2 row-start-1">
-            <img class="mb-5 lg:mb-0" src="<?php echo $image; ?>" alt="section_image">
+            <img class="mt-10 lg:mt-0 w-full relative" src="<?php echo $image; ?>" alt="section_image">
         </div>
     <?php endif; ?>
 
