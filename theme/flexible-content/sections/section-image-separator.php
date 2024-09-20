eb2312ab093ac1bd194b2bb9084bb9a6163c500a
Kamil <k.medrala99@gmail.com>
2024-09-20 16:03:59 +0200

    fullwidth image section

--- theme/flexible-content/sections/section-image-separator.php
+++ theme/flexible-content/sections/section-image-separator.php
@@ -3,16 +3,19 @@
 /** Template to display 'Sekcja ze zdjęciem' - section_image_separator */
 
 $image = $args['image'];
+$add_decor = $args['add_decor'];
 
 ?>
 
-<section class="relative pb-[100px] overflow-hidden">
+<section class="relative pb-[100px] ">
     <div class="w-full relative">
-        <svg class="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-full" xmlns="http://www.w3.org/2000/svg" width="150" height="75" viewBox="0 0 150 75" fill="none">
-            <circle cx="75" r="75" fill="#60B8D1" />
-        </svg>
+        <?php if($add_decor) : ?>
+            <svg class="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-full" xmlns="http://www.w3.org/2000/svg" width="150" height="75" viewBox="0 0 150 75" fill="none">
+                <circle cx="75" r="75" fill="#60B8D1" />
+            </svg>
+        <?php endif;?>
         <?php if ($image) : ?>
-            <img src="<?php echo $image; ?>" alt="image">
+            <img src="<?php echo $image; ?>" class="w-full object-cover max-h-[600px]" alt="image">
         <?php endif; ?>
     </div>
 </section>
