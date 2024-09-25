13846ef0f46ef94e93c5118c28e821efc5ccd3ec
Kamil <k.medrala99@gmail.com>
2024-09-25 12:29:20 +0200

    accesability fixes

--- theme/atcf7/cv.php
+++ theme/atcf7/cv.php
@@ -40,9 +40,8 @@ if (!defined('ABSPATH')) {
   <div class="max-w-[520px] mb-5 [&_a]:!text-white hover:[&_a]:!text-white [&_input]:!accent-secondary">
     <div class="mb-5 ">
       [acceptance gdpr_woo_consent]
-      <p>
         Wyrażam zgodę na przetwarzanie przez Polską Agencje Żeglugi Powietrznej z siedzibą przy ul. Wieżowej 8, 02-147 Warszawa, moich danych osobowych zawartych w dokumentacji aplikacyjnej w celu i zakresie niezbędnym do uczestnictwa w przyszłych procesach rekrutacji, w tym informowania mnie o realizowanych przez Polską Agencje Żeglugi Powietrznej rekrutacjach.
-      </p>
+      
       [/acceptance]
     </div>
   </div>
