c86eebc42b63fec4c6a097bd0d75ee73d192f214
Kamil <k.medrala99@gmail.com>
2024-09-25 14:00:44 +0200

    [PAZP-15] contact form

---
+++ theme/atcf7/contact.php
@@ -0,0 +1,42 @@
+<?php
+if (!defined('ABSPATH')) {
+  exit;
+}
+/**
+ * Template Name: CV
+ * */
+?>
+
+<div class="form-basic cv">
+  <div class="mb-[18px] grid grid-cols-1 lg:grid-cols-2 gap-x-[28px] gap-y-[10px]">
+    [text* your-name placeholder "Twoje imię*"]
+
+    [text* your-lastname placeholder "Twoje nazwisko*"]
+
+    [tel* your-phone placeholder "Numer telefon*"]
+
+    [email* your-email placeholder "Adres e-mail*"]
+
+    [text* your-title placeholder "Temat*"]
+
+  </div>
+
+  <div class="message-container w-full">
+    [textarea* your-message x3 placeholder "Treść wiadomości"]
+  </div>
+
+  <div class="max-w-[520px] mb-5 [&_a]:!text-white hover:[&_a]:!text-white [&_input]:!accent-secondary">
+    <div class="mb-5 ">
+      [acceptance gdpr_woo_consent]
+        Wyrażam zgodę na przetwarzanie przez Polską Agencje Żeglugi Powietrznej z siedzibą przy ul. Wieżowej 8, 02-147 Warszawa, moich danych osobowych zawartych w dokumentacji aplikacyjnej w celu i zakresie niezbędnym do uczestnictwa w przyszłych procesach rekrutacji, w tym informowania mnie o realizowanych przez Polską Agencje Żeglugi Powietrznej rekrutacjach.
+      [/acceptance]
+    </div>
+  </div>
+
+  <div class="btn-transparent group relative text-center max-w-full w-fit ml-auto [&_.wpcf7-spinner]:absolute [&_.wpcf7-spinner]:right-[-7px] [&_.wpcf7-spinner]:top-4 [&_svg_path]:hover:fill-secondary [&_svg_circle]:hover:stroke-secondary">
+    [submit "Wyślij"]
+    <svg class="absolute right-[26px] top-[16px] pointer-events-none" xmlns="http://www.w3.org/2000/svg" width="34" height="16" viewBox="0 0 34 16" fill="none">
+      <path class="group-hover:stroke-primary" d="M26.4003 14.5996L33 7.99996M33 7.99996L26.4003 1.4003M33 7.99996L1.49994 8.00003" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+  </div>
+</div>
---
+++ theme/flexible-content/sections/form-contact.php
@@ -0,0 +1,66 @@
+<?php
+
+/** Template to display 'Formularz z plikiem' - form_contact_file */
+
+$header = $args['header'];
+$description = $args['description'];
+$section_number = $args['section_number'];
+$title = $args['title'];
+$hide_message_field = $args['hide_message_field'];
+
+$image = $args['image'];
+
+if ($image['url']) {
+    $image_url = $image['url'];
+}
+
+?>
+
+<section>
+    <div class="w-full lg:h-[456px] overflow-visible relative py-10 md:py-[70px] mb:pb-20 bg-primary">
+        <div class="decoration absolute bottom-0 left-0">
+            <svg xmlns="http://www.w3.org/2000/svg" width="208" height="271" viewBox="0 0 208 271" fill="none">
+                <path d="M-60.0063 1.00488C-43.86 61.6364 57.9432 184.279 135.934 205.177C172.587 214.998 200.495 210.371 205.621 194.598C210.813 178.628 176.348 163.213 158.254 163.859C144.676 164.343 134.374 199.73 132.717 230.045C130.773 265.593 145.424 308.657 184.006 318.995" stroke="#60B8D1" stroke-width="2" />
+            </svg>
+        </div>
+        <div class="container">
+            <div class="cols-wrapper flex basis flex-col lg:flex-row">
+                <div class="col1 basis-1/2">
+                    <?php if ($section_number) : ?>
+                        <p class="w-fit mb-2 text-base leading-[24px] text-secondary font-semibold after:content-[''] after:w-[61px] after:h-[2px] after:bg-secondary after:rounded-full after:absolute after:top-1/2 after:-translate-y-1/2 after:right-[-70px] relative"><?php echo $section_number; ?></p>
+                    <?php endif; ?>
+
+                    <?php if ($title) : ?>
+                        <h2 class="mb-[50px] text-[50px] lg:text-[60px] text-white leading-[64px] lg:leading-[72px] font-medium z-[1] relative"><?php echo $title; ?></h2>
+                    <?php endif; ?>
+                </div>
+
+                <div class="col2 basis-1/2 z-[1]">
+                    <div class="mx-auto <?php if ($hide_message_field) {
+                                            echo '[&_.message-container]:hidden';
+                                        } ?>">
+
+                        <div class="form-cv-wrapper form-with-confirm-wrapper p-[40px_20px] lg:p-[40px_60px] bg-white text-textGray shadow-md lg:shadow-none">
+                            <p class="mb-[30px] text-[30px] leading-[36px] font-medium"> <?php esc_html_e('Send your CV', '_pansa'); ?></p>
+                            <?php echo do_shortcode('[contact-form-7 id="f8be224" title="CV"]'); ?>
+                            <div class="form-confirmation pointer-events-none opacity-0 z-10 absolute inset-0 flex flex-col items-center justify-center transition duration-300 text-white">
+                                <svg class="max-w-full mb-4" width="125" height="125" viewBox="0 0 125 125" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <circle cx="62.5" cy="62.5" r="60.5" stroke="#ffffff" stroke-width="4"></circle>
+                                    <path d="M38.5713 62.5L54.2856 77.8571L85.7141 47.1428" stroke="#ffffff" stroke-width="8" stroke-linecap="round" stroke-linejoin="round"></path>
+                                </svg>
+                                <h3 class="text-center text-2xl md:leading-10 md:!text-[40px] max-w-[460px] font-bold mb-5"><?php esc_html_e('Thank you for sending your message', '_pansa'); ?></h3>
+                                <p class="text-center text-base max-w-[460px] mb-10 md:mb-16"><?php esc_html_e('Our experts are already verifying your message, we will get back to you soon with the information you need', '_pansa'); ?></p>
+                                <button data-js-form-reset="form-cv-wrapper" class="w-full max-w-[520px] border-none !bg-white hover:!bg-secondary hover:!text-white transition-all duration-200 !text-foreground h-[55px] !px-5 xl:!px-12 !rounded-[15px] font-semibold !flex items-center justify-center">
+                                    <?php esc_html_e('Go back to form', '_pansa'); ?>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php if($image_url){
+        echo pansa_img_responsive($image, 'w-full', array(1440, 500), 'lazy');
+    } ?>
+</section>
