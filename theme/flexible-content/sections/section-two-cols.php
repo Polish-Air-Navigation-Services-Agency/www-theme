f5f34f5dac7a1ee443646265fb1a961a3d1584dc
Kamil <k.medrala99@gmail.com>
2024-09-27 09:19:28 +0200

    [PAZP-19] two-cols btn margin top

--- theme/flexible-content/sections/section-two-cols.php
+++ theme/flexible-content/sections/section-two-cols.php
@@ -40,7 +40,7 @@ $overlapping_background = $args['overlapping_background'];
             <?php endif; ?>
 
             <?php if ($button) : ?>
-                <button class="mb-[30px] lg:mb-0 text-[18px] font-medium bg-white !rounded-full border border-[#BEBFBF] !px-6 !py-[11px] text-[#0F304D] hover:bg-primary hover:text-white hover:border-primary transition ease duration-200">
+                <button class="mb-[30px] mt-8 lg:mt-[50px] lg:mb-0 text-[18px] font-medium bg-white !rounded-full border border-[#BEBFBF] !px-6 !py-[11px] text-[#0F304D] hover:bg-primary hover:text-white hover:border-primary transition ease duration-200">
                     <a href="<?php echo esc_url(home_url('/o-nas')); ?>" class="flex items-center justify-center gap-[10px]">
                         <?php echo $button; ?>
                     </a>
