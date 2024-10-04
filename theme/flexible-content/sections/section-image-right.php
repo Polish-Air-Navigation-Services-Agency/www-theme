71e4f188103067d5d2f5b529acd7f03e3bf61c1f
Kamil <k.medrala99@gmail.com>
2024-10-04 13:39:28 +0200

    img right z-index fix

--- theme/flexible-content/sections/section-image-right.php
+++ theme/flexible-content/sections/section-image-right.php
@@ -6,9 +6,9 @@ $image = $args['image'];
 
 ?>
 
-<section class="relative overflow-hidden">
+<section class="section_image_right">
     <div class="container">
-        <div class="mr-[calc(50%_-_50vw)] h-[300px] lg:h-[520px] relative z-20" data-js-parallax="container">
+        <div class="mr-[calc(50%_-_50vw)] h-[300px] lg:h-[520px]" data-js-parallax="container">
             <div data-js-parallax="img">
                 <?php if (isset($image['url'])){
                     echo pansa_img_responsive($image, 'h-full w-full object-cover', array(1536, 520), 'eager');
