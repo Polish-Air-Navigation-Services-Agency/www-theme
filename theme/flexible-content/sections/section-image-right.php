13b15dfae9e62175384de7229aa1b8b3cbb0b64f
Kamil <k.medrala99@gmail.com>
2024-10-04 12:20:22 +0200

    svg decor positions, img-right parallax fix

--- theme/flexible-content/sections/section-image-right.php
+++ theme/flexible-content/sections/section-image-right.php
@@ -8,10 +8,10 @@ $image = $args['image'];
 
 <section class="relative overflow-hidden">
     <div class="container">
-        <div class="mr-[calc(50%_-_50vw)] relative z-10" data-js-parallax="container">
+        <div class="mr-[calc(50%_-_50vw)] h-[300px] lg:h-[520px] relative z-10" data-js-parallax="container">
             <div data-js-parallax="img">
                 <?php if (isset($image['url'])){
-                    echo pansa_img_responsive($image, 'h-[300px] lg:h-[520px] w-full object-cover', array(1536, 520), 'eager');
+                    echo pansa_img_responsive($image, 'h-full w-full object-cover', array(1536, 520), 'eager');
                 }; ?>
             </div>
         </div>
