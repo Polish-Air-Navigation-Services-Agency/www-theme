2a20b24f322f92943c385d2e0e1a495da6f89500
Kamil <k.medrala99@gmail.com>
2024-11-25 10:36:03 +0100

    section 2 img separator fullwidth

---
+++ theme/flexible-content/sections/section-two-image-separator.php
@@ -0,0 +1,35 @@
+<?php
+
+/** Template to display 'Sekcja z 2 zdjęciami' - section_two_image_separator */
+
+$image_left = $args['image_left'];
+$image_right = $args['image_right'];
+$add_decor = $args['add_decor'];
+
+?>
+
+<section class="relative <?php if($add_decor) : ?> pb-[100px] <?php endif;?>">
+    <div class="w-full relative">
+        <?php if($add_decor) : ?>
+            <svg class="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-full" xmlns="http://www.w3.org/2000/svg" width="150" height="75" viewBox="0 0 150 75" fill="none">
+                <circle cx="75" r="75" fill="#60B8D1" />
+            </svg>
+        <?php endif;?>
+        <div class="md:aspect-[2048/600] grid grid-cols-1 md:grid-cols-2">
+            <div data-js-parallax="container" class="max-h-[700px]">
+                <div data-js-parallax="img" class="w-full h-full">
+                    <?php if (isset($image_left['url'])){
+                            echo pansa_img_responsive($image_left, 'h-full w-full object-cover', array(1024, 700), 'lazy');
+                    }; ?>
+                </div>
+            </div>
+            <div data-js-parallax="container" class="max-h-[700px]">
+                <div data-js-parallax="img" class="w-full h-full">
+                    <?php if (isset($image_right['url'])){
+                            echo pansa_img_responsive($image_right, 'h-full w-full object-cover', array(1024, 700), 'lazy');
+                    }; ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
